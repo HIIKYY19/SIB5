e786e34ddfcc9e24449ef070439c2936ec99efa1
Muhammad Rizky Fauzi <143673275+HIIKYY19@users.noreply.github.com>
2023-09-21 20:10:22 -1200

    Add files via upload

---
+++ tugas1php/layoutit/src/Portofolio.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PORTOFOLIO</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <style>
+            body{
+                background: radial-gradient(#5CD2E6, #3085C3, #F1EFEF);
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item">
+                                <a href="index.php">HOME</a>
+                            </li>
+                            <li class="breadcrumb-item">
+                                <a href="Portofolio.php">PORTOFOLIO</a>
+                            </li>
+                            <li class="breadcrumb-item">
+                                <a href="https://www.instagram.com/_hi__iky___/">CONTACT</a>
+                            </li>
+                        </ol>
+                    </nav>
+            <h1>About Me</h1>
+            <img src="IKYY.jpg" height="400" width="400">
+    </body>
+</html>
+
+<?php
+echo "<br>";
+echo "Nama : Muhammad Rizkiy Fauzi <br>";
+echo "Pendidikan :<br> - SD<br> - SMP<br> - SMA<br>";
+echo "Hoby : <br>- Menonton<br> - Bermain Game<br> - Membaca<br>";
+?>

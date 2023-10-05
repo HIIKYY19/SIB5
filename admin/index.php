8a11197892109730526403e1f0a34318844cb84a
HIIKYY19 <nurrohmanrizky23@gmail.com>
2023-10-04 20:36:47 -1200

    menambahkan admin Dashboard

---
+++ admin/index.php
@@ -0,0 +1,12 @@
+<?php
+include_once 'top.php';
+include_once 'menu.php';
+?>
+<div>
+    <div class="container-fluid px-4">
+        <h1>Selamat Datang Di Halaman Admin</h1>
+    </div>
+</div>
+<?php
+include_once 'buttom.php';
+?>
